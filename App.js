18fd5bced4ae16cf012b3076bde47b59ccc249a1
Nikki Than Win <thanwinnikki@gmail.com>
2021-05-28 22:47:20 +0800

    Add Sign-In pages

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import WelcomePage from './src/WelcomePage'
+import SignupPage from './src/SignupPage'
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    
+    <WelcomePage />
+    
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
