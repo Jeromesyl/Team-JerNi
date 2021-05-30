d27310ef8a4e323510df4bec5ae2caedbd0d2b78
thanwinnikki <84826532+thanwinnikki@users.noreply.github.com>
2021-05-30 15:46:06 +0800

    Navigation (#1)
    * added basic home screen
    
    * Added navigation stack with test screens
    
    * Added navigation stack with test screens
    
    * Implemented firebase authentication for login and register
    
    * Homescreen now displays username
    
    Co-authored-by: Jerome Soh <jsyl3214@gmail.com>

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import WelcomePage from './src/WelcomePage'
-import SignupPage from './src/SignupPage'
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import StartScreen from "./src/screens/StartScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import SignupPage from "./src/screens/SignupPage";
+import LoginPage from "./src/screens/LoginPage";
+const Stack = createStackNavigator();
 
 export default function App() {
-  
   return (
-    
-    <WelcomePage />
-    
-  )
-};
-
+    <NavigationContainer initialRouteName="Start">
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { CommonActions } from "@react-navigation/native";
+
+import * as Authentication from "../../api/auth";
+
+export default function HomeScreen({ route, navigation }) {
+  const { name } = route.params;
+
+  const handleLogout = () => {
+    Authentication.signOut(
+      () =>
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{ name: "Login" }],
+          })
+        ),
+      console.error
+    );
+  };
+
+  return (
+    <View style={styles.background}>
+      <View style={styles.profile}>
+        <Text>Home Screen</Text>
+        <Text>Welcome {name}</Text>
+        <Button title="Sign out" onPress={handleLogout} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#FF5858",
+  },
+  profile: {
+    flex: 0.5,
+    backgroundColor: "white",
+    height: 50,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
